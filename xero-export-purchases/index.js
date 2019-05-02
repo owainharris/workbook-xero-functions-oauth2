f26eb818bb532a3bafef5924b63dad968aa29bbc
Owain Harris <you@example.com>
2019-05-02 19:35:54 -0400

    Added xero purchases export

--- xero-export-purchases/index.js
+++ xero-export-purchases/index.js
@@ -4,26 +4,27 @@ module.exports = async function(context, req) {
   const oauth_token = req.body.authToken;
   delete req.body.authToken;
   // Remove Total as Xero calculates this on import
-  for (var i = 0; i < req.body.invoices.length; i++) {
-    if (req.body.invoices[i].Total !== undefined) {
-      delete req.body.invoices[i].Total;
+  for (var i = 0; i < req.body.purchaseOrders.length; i++) {
+    if (req.body.purchaseOrders[i].Total !== undefined) {
+      delete req.body.purchaseOrders[i].Total;
     }
   }
-  // Connect to Xero
+  // connect to xero
   let xero = new XeroClient(xeroConfig, oauth_token);
 
-  // Send request data to the Xero invoices API and wait for a response
+  // Send request data to the Xero purchases API and wait for a response
   try {
-    let result = await xero.invoices.create(req.body);
-    // Check and filter for returned response errors or warnings for each invoice
-    let issues = await result.Invoices.map(e => {
+    let result = await xero.purchaseOrders.create(req.body);
+    // Check and filter for returned response errors or warnings for each purchases
+    let issues = result.PurchaseOrders.map(e => {
       return {
-        invoice: e.InvoiceNumber,
+        purchase: e.PurchaseOrderNumber,
         errors: e.ValidationErrors,
         warnings: e.Warnings
       };
     }).filter(obj => obj);
     let response = await issues;
+
     // Send returned data from Xero including errors or warnings
     context.res = {
       status: 200,
