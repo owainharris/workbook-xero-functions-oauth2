5f2f94724897700db6ffae425276a032e2eb13ad
owainh@gmail.com <owainh@gmail.com>
2019-12-17 15:38:22 -0500

    Created draft Creditor Invoices sync

--- hook-test/index.js
+++ hook-test/index.js
@@ -5,33 +5,79 @@ module.exports = async function(context, req) {
   const baseURL = req.headers.baseurl;
   const id = req.body.Id;
   const auth = req.headers.auth;
-  const jobEndPoint = `/api/job/${id}`;
+  const nuintyAuth = req.headers.nuintyauth;
+  const nuityBaseURL = req.headers.nuitybaseurl;
+  const creditorEndPoint = `/api/finance/account/creditor/${id}`;
   const proxyURL = "https://immense-shore-64867.herokuapp.com/";
+  const nuityEndPoint = "/api/creditors/save";
 
   const headers = {
     headers: {
       Authorization: `${auth}`,
       Accept: "application/json",
-      "Content-type": "application/json"
-      // "Access-Control-Allow-Credentials": "true",
-      // "Access-Control-Allow-Origin": "*",
-      // "X-Requested-With": "application/json"
+      "Content-type": "application/json",
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Origin": "*",
+      "X-Requested-With": "application/json"
     }
   };
 
-  let jobResult = await axios.get(baseURL + jobEndPoint, headers);
+  const nuityHeaders = {
+    headers: {
+      Authorization: `${nuintyAuth}`,
+      Accept: "application/json",
+      "Content-type": "application/json"
+    }
+  };
 
-  context.log(JSON.stringify("Job Result " + jobResult));
-
-  const JobTypeId = jobResult.data.JobTypeId;
-  const jobStatusEndPoint = `/api/settings/job/type/${JobTypeId}`;
-
-  let jobStatusResult = await axios.get(
-    proxyURL + baseURL + jobStatusEndPoint,
+  // GET CREDITOR DATA IN WORKBOOK BY ID
+  const creditorResult = await axios.get(
+    proxyURL + baseURL + creditorEndPoint,
     headers
   );
 
-  jobStatusResult.data.Name === "Media"
-    ? context.log("IT IS MEDIA, LET's DO STUFF AND POST TO NUNITY!")
-    : context.log("NOT A MEDIA JOB, DO NOTHING");
+  context.log("WB Response: " + creditorResult);
+
+  // PUT RETURNED CREDITOR OBJECT INTO AN ARRAY SO WE CAN USE THE MAP METHOD
+  let creditorResultArray = [];
+  creditorResultArray.push(creditorResult.data);
+
+  context.log("ARRAY: " + creditorResultArray);
+
+  //CREATE CREITOR MODEL TO POST IN NUINTY
+  const creditorModel = creditorResultArray.map(i => {
+    return {
+      creditorNumber: i.Id,
+      mediaType: "5",
+      mediaClass: "50",
+      countryCode: "AUS",
+      mediaState: "02",
+      marketCode: "S",
+      name: i.Name,
+      addressLine1: i.Address1,
+      addressLine2: "Test address 2",
+      addressLine3: "Test address 3",
+      addressLine4: "Test address 4",
+      postCode: "2000",
+      contactName: "Joe Bloggs",
+      telNumber: "02123456",
+      emailAddress: "aaa@bbb.com",
+      creditorClass: "1",
+      currencyCode: "AUD",
+      gstStatus: "E",
+      taxIdentificationNumber: "77777",
+      withholdingTaxCode: "C100",
+      netOrGrossFlag: "G",
+      abnNumber: "",
+      defaultDiscountPercent: 5.5,
+      defaultCommissionPercent: 6.6
+    };
+  });
+
+  const postToNuity = await axios.post(
+    "https://" + nuityBaseURL + nuityEndPoint,
+    JSON.stringify(creditorModel),
+    nuityHeaders
+  );
+  context.log("Nuity Response: " + JSON.stringify(postToNuity));
 };
